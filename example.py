5a9f4a3dbd6492cb9cf85544a0aeeede6cc3f55c
ballakundana-spec <ballakundana@gmail.com>
2025-11-27 09:06:52 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,5 @@
+print("welcome to tia")
+a=10
+b=25
+result=a+b
+print("addition of",a,b, "is" ,result)
---
+++ example2.py
@@ -0,0 +1,5 @@
+print("welcome to nakshatra pep talk")
+a=20
+b=50
+result=a+b
+print("addition of",a,b,"is" ,result)
